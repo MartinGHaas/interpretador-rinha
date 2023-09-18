313f6445df6b5428843c351fc5a0e043deb650ae
Martin Haas <martinhaas2005@gmail.com>
2023-09-18 00:54:30 -0300

    Add Base Op & If

--- interpreter.js
+++ interpreter.js
@@ -6,7 +6,7 @@ function interpreter(node, environment) {
     case 'Str':
       console.log('ENTROU NA TIPAGEM');
       return node.value;
-    // Método Print
+    // Alterar caso print(print(x))
     case 'Print':
       console.log('ENTROU NO PRINT');
       const term = interpreter(node.value, environment);
@@ -19,15 +19,40 @@ function interpreter(node, environment) {
       console.log('ENTROU NO VAR');
       return environment[node.text];
     case 'Binary':
-      const rhs = interpreter(node.rhs, environment);
       const lhs = interpreter(node.lhs, environment);
+      const rhs = interpreter(node.rhs, environment);
       switch(node.op) {
         case 'Add':
-          return rhs + lhs;
+          return lhs + rhs;
+        case 'Sub':
+          return lhs - rhs;
+        case 'Mul': 
+          return lhs * rhs;
+        case 'Div':
+          return Math.floor(lhs / rhs); // Consertar divisões por 0 | O resultado é arredondado para o menor numero
+        case 'Rem':
+          return lhs / rhs;
+        case 'Eq':
+          return lhs === rhs;
+        case 'Neq':
+          return lhs !== rhs;
+        case 'Lt':
+          return lhs < rhs;
+        case 'Gt':
+          return lhs > rhs;
+        case 'Lte':
+          return lhs <= rhs;
+        case 'Gte':
+          return lhs >= rhs;
+        case 'And':
+          return lhs && rhs;
+        case 'Or':
+          return lhs || rhs;
         default: 
           console.log('Nenhuma Operação Encotrada');
+        // TODO: Ajeitar casos de booleanos para entradas SOMENTE booleanas
       }
-      
+    case 'If':
       return;
     default:
       console.log('Unrecognized Term');
